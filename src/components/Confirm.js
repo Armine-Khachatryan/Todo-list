92291f6e7524fca7cf34bb2f529105d415e3e2a0
Armine-Khachatryan <arminehkhachatryan@gmail.com>
2021-01-26 21:41:41 +0400

    new task using modal and edit button

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -1,37 +1,33 @@
 import React from 'react';
-import {Modal, Button} from 'react-bootstrap';
-import PropTypes from "prop-types";
-function Confirm (props){
+import { Modal, Button } from 'react-bootstrap';
+
+function Confirm(props) {
   return (
     <Modal
-    show={true}
-    onHide={props.onClose}
-  size="lg"
-  aria-labelledby="contained-modal-title-vcenter"
-  centered
->
-<Modal.Header closeButton>
+      show={true}
+      onHide={props.onClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Are you sure to remove {props.count} task{props.count>1 ? "s": ""}
+          Are you sure to remove {props.count} task{props.count > 1 ? "s" : ""}
         </Modal.Title>
       </Modal.Header>
       <Modal.Footer>
-        <Button 
-        onClick={props.onConfirm}
-        variant='danger'
+        <Button
+          onClick={props.onConfirm}
+          variant='danger'
         >
-        Delete
+          Delete
         </Button>
         <Button onClick={props.onClose}>Cancel</Button>
       </Modal.Footer>
     </Modal>
-    )
+  )
 }
-    
-Confirm.propTypes={
-onClose:PropTypes.func.isRequired,
-onConfirm:PropTypes.func.isRequired,
-count:PropTypes.number.isRequired
-}
+
+
 
 export default Confirm;
